f9a77bf7744751257da0dd72ac38c28ab75da2e6
Ambarish Ganguly <ambarish.ganguly@gmail.com>
2025-01-21 10:44:34 +0530

    Initial Commit

---
+++ git_deploy.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin http://github.com/ambarishg/misron-consulting-azure.git
+git push -u origin main
---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import theme from "./theme";
+import Partners from "./components/Partners";
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Router basename="/misronconsulting"> {/* Set the basename for routing */}
+        <Box minHeight="100vh" display="flex" flexDirection="column">
+          <Header />
+          <Box flexGrow={1}>
+            <Routes>
+              {/* Redirect root path to external URL */}
+              <Route path="/" element={<Home/>} />
+              <Route path="/misronconsulting" element={<Home/>} />
+              
+              {/* Define other routes */}
+              <Route path="/about" element={<About />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/partners" element={<Partners />} />
+            </Routes>
+          </Box>
+          <Footer />
+        </Box>
+      </Router>
+    </ChakraProvider>
+  );
+}
+
+export default App;
