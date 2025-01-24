beec6871e0923868d8dcde4db5a85c124b3aae12
Ambarish Ganguly <ambarish.ganguly@gmail.com>
2025-01-24 21:12:18 +0530

    Static Web

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ChakraProvider, Box } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
